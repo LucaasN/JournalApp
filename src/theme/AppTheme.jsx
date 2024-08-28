2e10392b471dceb6d28932fd39c6fda8cd252ac0
lucas.nunez@7r1ck.com <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>
2024-08-28 11:57:04 -0300

    First commit JournalApp: react + materialui (solo maqueta)

---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import {purpleTheme} from './';
+
+export const AppTheme = ({ children }) => {
+  return (
+    <ThemeProvider theme={ purpleTheme }>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+
+      {children}
+    </ThemeProvider>
+  )
+}
